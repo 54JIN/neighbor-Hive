9e1cf52f7d2fd080d7a38b393068ab4ef9856225
Sajin Saju <sajinsaju2002@gmail.com>
2021-01-22 02:12:21 -0500

    add method

--- foodSale.java
+++ foodSale.java
@@ -1,3 +1,35 @@
 public class foodSale{
+    /*
+    **Linked List with a mixture of Stack concept: insert time complexity of O(1)
+    */
+    //data
+    String foodName;
+    double foodPrice;
+    boolean priceHaggle;
+
+    //pointers
+    foodSale head;
+    foodSale previous;
+
+    public foodSale(){
+        this.head = null;
+        this.previous = null;
+    }
+    public foodSale(String foodName, double foodPrice, boolean priceHaggle){
+        this.foodName = foodName;
+        this.foodPrice = foodPrice;
+        this.priceHaggle = priceHaggle;
+        this.previous = null;
+    }
+    public void add(String foodName, double foodPrice, boolean priceHaggle){
+        foodSale temp = new foodSale(foodName, foodPrice, priceHaggle);
+        if(head == null){
+            head = temp;
+        }
+        else{
+            temp.previous = head;
+            head = temp;
+        }
+    }
     
 }
