c6660b750cf33139656d8f855a2e1bd59db70953
Sajin Saju <sajinsaju2002@gmail.com>
2021-01-24 03:03:30 -0500

    updated sellers as BST

--- sellers.java
+++ sellers.java
@@ -1,16 +1,16 @@
-public class sellers{
+import java.util.Scanner;
 
+public class sellers {
+    /*
+    $$Description
+    **
+    $$TO DO:
+    **Hashing method, add method, get method?, set method
+    **get method: list of food, food in question, list of drinks, drink in question, list of confiments, confiment in question
+    */
     //Sellers information
-    String sellerName;
-    String sellerAddress;
-    String sellerCity;
-    String sellerState;
-    int sellerZip;
-
-    //Sellers goods
-    foodSale avaFoods;
-    drinkSale avaDrinks;
-    condimentSale avaCondiments;
+    sellerData sellerContent;
+    int sellerID;
 
     //Binary Tree information
     sellers root;
@@ -23,25 +23,138 @@ public class sellers{
         this.rightTree = null;
     }
     //Initializations for class with information
-    public sellers(String sellerName, String sellerAddress, String sellerCity, String sellerState, int sellerZip){
-        this.sellerName = sellerName;
-        this.sellerAddress = sellerAddress;
-        this.sellerCity = sellerCity;
-        this.sellerState = sellerState;
-        this.sellerZip = sellerZip;
+    public sellers(int val){
+        this.sellerID = val;
         this.leftTree = null;
         this.rightTree = null;
     }
-
+    //add seller to the binary tree recursively
+    public void addBinaryTree(sellers root, sellerData tempData, int val){
+        if(root.sellerID == val){
+            System.out.println("check: ");
+            root.sellerContent.addData(tempData);
+        }
+        else if(root.leftTree == null && val < root.sellerID){
+            sellers temp = new sellers(val);
+            temp.sellerContent = new sellerData();
+            temp.sellerContent.addData(tempData);
+            root.leftTree = temp;
+        }
+        else if(root.rightTree == null && val > root.sellerID){
+            sellers temp = new sellers(val);
+            temp.sellerContent = new sellerData();
+            temp.sellerContent.addData(tempData);
+            root.rightTree = temp;
+        }
+        else if(val < root.sellerID){
+            addBinaryTree(root.leftTree, tempData, val);
+        }
+        else if(val > root.sellerID){
+            addBinaryTree(root.rightTree, tempData, val);
+        }
+    }
+    //return the hashVal of the data provided
+    public int hashVal(String sellerFirstName, String sellerLastName, String sellerAddress, int sellerZip){
+        return (int)(Math.abs((sellerFirstName + sellerLastName + sellerAddress).hashCode()/sellerZip));
+    }
+    //add sellers
+    public void addSeller(String sellerFirstName, String sellerLastName, String sellerAddress, String sellerCity, String sellerState, int sellerZip){
+        int val = (int)(Math.abs((sellerFirstName + sellerLastName + sellerAddress).hashCode()/sellerZip));
+        sellerData tempData = new sellerData(sellerFirstName, sellerLastName, sellerAddress, sellerCity, sellerState, sellerZip);
+        //if the root is empty
+        if(root == null){
+            sellers temp = new sellers(val);
+            temp.sellerContent = new sellerData();
+            temp.sellerContent.addData(tempData);
+            root = temp;
+        }
+        else{
+            addBinaryTree(root, tempData, val);
+        }
+    }
+    //search the tree for a specific ID
+    public sellerData searchTree(sellers root,int ID){
+        if(root != null){
+            if(root.sellerID == ID){
+                return root.sellerContent;
+            }
+            else if(ID < root.sellerID){
+                return searchTree(root.leftTree, ID);
+            }
+            else if(ID > root.sellerID){
+                return searchTree(root.rightTree, ID);
+            }
+        }
+        return null;
+    }
+    //checks if the sellers is within the binary tree
+    public boolean containsSeller(String sellerFirstName, String sellerLastName, String sellerAddress, String sellerCity, String sellerState, int sellerZip){
+        int ID = hashVal(sellerFirstName, sellerLastName, sellerAddress, sellerZip);
+        sellerData tempData = searchTree(root, ID);
+        
+        return tempData == null? false : tempData.containsData(sellerFirstName, sellerLastName, sellerAddress, sellerCity, sellerState, sellerZip);
+    }
     public static void main(String[] args){
-        String name = "Sajin";
-        String name2 = "Zihe zhang 35 trump ave bergenfield nj 07621";
-        String name3 = "Sajin Saju 45 Melrose ave bergenfield nj 07621";
-        int temp = name.hashCode();
-        long temp2 = Math.abs(name2.hashCode()/name2.length());
-        long temp3 = Math.abs(name3.hashCode()/name3.length());
-        System.out.println(temp);
-        System.out.println(temp2);
-        System.out.println(temp3);
+        sellers temp = new sellers();
+        
+        String sellerFirstName = "Sajin";
+        String sellerLastName = "Saju";
+        String sellerAddress = "45 Melrose Ave";
+        String sellerCity = "Bergenfield";
+        String sellerState = "NJ";
+        int sellerZip = 07621;
+        temp.addSeller(sellerFirstName, sellerLastName, sellerAddress, sellerCity, sellerState, sellerZip);
+        System.out.println(temp.containsSeller(sellerFirstName, sellerLastName, sellerAddress, sellerCity, sellerState, sellerZip));
+        sellerFirstName = "Peter";
+        sellerLastName = "Zhang";
+        sellerAddress = "35 Melrose Ave";
+        sellerCity = "Bergenfield";
+        sellerState = "NJ";
+        sellerZip = 07621;
+        temp.addSeller(sellerFirstName, sellerLastName, sellerAddress, sellerCity, sellerState, sellerZip);
+        System.out.println(temp.containsSeller(sellerFirstName, sellerLastName, sellerAddress, sellerCity, sellerState, sellerZip));
+        
+        /*
+        Scanner scan = new Scanner(System.in);
+        
+        boolean loop = true;
+        while(loop){
+            System.out.println("Input: ");
+            String val = scan.nextLine();
+            if(val.equals("stop")){
+                loop = false;
+            }
+            else if(val.equals("add seller")){
+                System.out.println("First Name: ");
+                String sellerFirstName = scan.nextLine();
+                System.out.println("Last Name: ");
+                String sellerLastName = scan.nextLine();
+                System.out.println("Address: ");
+                String sellerAddress = scan.nextLine();
+                System.out.println("City: ");
+                String sellerCity = scan.nextLine();
+                System.out.println("State: ");
+                String sellerState = scan.nextLine();
+                System.out.println("Zip: ");
+                int sellerZip = Integer.parseInt(scan.nextLine());
+                temp.addSeller(sellerFirstName, sellerLastName, sellerAddress, sellerCity, sellerState, sellerZip);
+            }
+            else if(val.equals("contains seller")){
+                System.out.println("First Name: ");
+                String sellerFirstName = scan.nextLine();
+                System.out.println("Last Name: ");
+                String sellerLastName = scan.nextLine();
+                System.out.println("Address: ");
+                String sellerAddress = scan.nextLine();
+                System.out.println("City: ");
+                String sellerCity = scan.nextLine();
+                System.out.println("State: ");
+                String sellerState = scan.nextLine();
+                System.out.println("Zip: ");
+                int sellerZip = Integer.parseInt(scan.nextLine());
+                System.out.println(temp.containsSeller(sellerFirstName, sellerLastName, sellerAddress, sellerCity, sellerState, sellerZip));
+            }
+        }
+        */
     }
 }
